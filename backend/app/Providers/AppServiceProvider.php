ba47fa42f29a7155a6edfff542c5b4144f28ff5f
MMXXXII <perfilev621@gmail.com>
2026-07-16 12:56:35 +0800

    Добавлены требования к сложности пароля

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
     {
         View::composer('*', function ($view) {
             $view->with('sidebarCategories', Category::orderBy('order')->get());
+        });
+
+        Password::defaults(function () {
+            return Password::min(8)->mixedCase()->numbers();
         });
     }
 }
