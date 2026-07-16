3aa5e0eae429cdf74c5fc8429592148ab284a52e
MMXXXII <perfilev621@gmail.com>
2026-07-16 11:01:12 +0800

    Добавлена модель Topic

---
+++ backend/app/Models/Topic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'user_id', 'title', 'slug', 'is_pinned', 'is_locked'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
